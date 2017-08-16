8bd2fc5f7048f906ccb859404d6b55228e3b8f9b
Avik Datta <avik.datta@imperial.ac.uk>
2017-08-16 17:00:59 +0100

    listing all dirs present in the target path

--- scripts/seqrun_processing/find_new_seqrun_and_prepare_md5.py
+++ scripts/seqrun_processing/find_new_seqrun_and_prepare_md5.py
@@ -2,7 +2,25 @@ import os, hashlib
 from igf_data.igfdb.baseadaptor import BaseAdaptor
 from igf_data.igfdb.seqrunadaptor import SeqrunAdaptor
 
-def find_new_seqrun_dir(
+def find_new_seqrun_dir(path, dbconfig):
+  '''
+  A method for check and finding new sequencing run directory
+  '''
+  all_seqrun_dir=[f for f in os.listdir(path) if os.path.isdir(os.path.join(path,f))]    # list of all directories present under path
+  new_seqrun_dir=check_seqrun_dir_in_db(all_seqrun_dir,dbconfig)                          
+  valid_seqrun_dir=check_finished_seqrun_dir(new_seqrun_dir)
+  return valid_seqrun_dir
+
+def check_seqrun_dir_in_db(all_seqrun_dir,dbconfig):
+  '''
+  A method for checking existing seqrun dirs in database
+  required params:
+  all_seqrun_dir: list of seqrun dirs to check
+  dbconfig: dbconfig
+  '''
+  pass  
+  
+
 def calculate_file_checksum(filepath, hasher='md5'):
   '''
   A method for file checksum calculation
