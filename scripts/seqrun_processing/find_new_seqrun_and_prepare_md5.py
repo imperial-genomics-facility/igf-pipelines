55b832f8b4b47aa307c7f3bd7c943a2e9dabddf1
Avik Datta <avik.datta@imperial.ac.uk>
2017-08-21 16:31:35 +0100

    added parameter parsing for seqrun processing script

--- scripts/seqrun_processing/find_new_seqrun_and_prepare_md5.py
+++ scripts/seqrun_processing/find_new_seqrun_and_prepare_md5.py
@@ -1,14 +1,16 @@
-from igflib.process.seqrun_processing.find_and_process_new_seqrun import find_new_seqrun_dir, calculate_file_md5, load_seqrun_files_to_db
+import argparse
+from igf_data.process.seqrun_processing.find_and_process_new_seqrun import find_new_seqrun_dir, calculate_file_md5, load_seqrun_files_to_db
 
-if __name__=='__main__':
-  path='/home/vmuser/git_code/igf-pipelines/data/seqrun_dir'
-  md5_out_path='/home/vmuser/git_code/test_dir'
-  #checksum=calculate_file_checksum(filepath='/home/vmuser/git_code/data-management-python/doc/data/Illumina/RunInfo.xml')
-  #print(checksum)
+parser=argparse.ArgumentParser()
+parser.add_argument('-p','--seqrun_path', required=True, help='Seqrun directory path')
+parser.add_argument('-m','--md5_path', required=True, help='Seqrun md5 output dir')
+parser.add_argument('-d','--dbconfig_path', required=True, help='Database configuration json file')
+args=parser.parse_args()
 
-  dbconfig='/home/vmuser/git_code/igf-pipelines/data/dbconfig.json'
-  new_seqrun=find_new_seqrun_dir(path, dbconfig)
-  new_seqrun_and_md5=calculate_file_md5(seqrun_info=new_seqrun, md5_out=md5_out_path)
-  load_seqrun_files_to_db(seqrun_info=new_seqrun, seqrun_md5_info=new_seqrun_and_md5, dbconfig=dbconfig)
-  
+seqrun_path=args.seqrun_path
+md5_path=args.md5_path
+dbconfig_path=args.dbconfig_path=
 
+new_seqruns=find_new_seqrun_dir(seqrun_path, dbconfig_path)
+new_seqrun_files_and_md5=calculate_file_md5(seqrun_info=new_seqruns, md5_out=md5_path)
+load_seqrun_files_to_db(seqrun_info=new_seqruns, seqrun_md5_info=new_seqrun_files_and_md5, dbconfig=dbconfig_path)
