6b8170235c6248f3dad297941f13604b63435f95
Avik Datta <avik.datta@imperial.ac.uk>
2017-08-21 15:46:08 +0100

    updated seqrun processing package and fixed filemd5 calculation function

--- test/test_find_and_process_new_seqrun.py
+++ test/test_find_and_process_new_seqrun.py
@@ -1,23 +1,26 @@
-import unittest, json
+import unittest, json, os, shutil
 from sqlalchemy import create_engine
+from igf_data.igfdb.igfTables import Base
 from igf_data.igfdb.baseadaptor import BaseAdaptor
 from igf_data.igfdb.platformadaptor import PlatformAdaptor
 from igf_data.igfdb.seqrunadaptor import SeqrunAdaptor
 from igflib.process.seqrun_processing.find_and_process_new_seqrun import find_new_seqrun_dir,check_finished_seqrun_dir,check_seqrun_dir_in_db,calculate_file_md5,calculate_file_checksum,prepare_seqrun_for_db,load_seqrun_files_to_db
 
 class Find_seqrun_test1(unittest.TestCase):
-  def setup(self):
-    self.path='../data/seqrun_dir'
-    self.dbconfig='../data/dbconfig.json'
-    self.md5_out_path='../data'
-    seqrun_json='../data/seqrun_db_data.json'
-    platform_json='../data/platform_db_data.json'
+  def setUp(self):
+    self.path = 'data/seqrun_dir'
+    self.dbconfig = 'data/dbconfig.json'
+    self.md5_out_path = 'data/md5_dir'
+    seqrun_json = 'data/seqrun_db_data.json'
+    platform_json = 'data/platform_db_data.json'
 
-    dbparam=None
-    with open(dbconfig, 'r') as json_data:
-      dbparam=json.load(json_data)
-    base=BaseAdaptor(**dbparam)
-    self.engine=base.engine
+    os.mkdir(self.md5_out_path)
+    dbparam = None
+    with open(self.dbconfig, 'r') as json_data:
+      dbparam = json.load(json_data)
+    base = BaseAdaptor(**dbparam)
+    self.engine = base.engine
+    self.dbname=dbparam['dbname']
     Base.metadata.create_all(self.engine)
     base.start_session()
 
@@ -26,13 +29,36 @@ class Find_seqrun_test1(unittest.TestCase):
       pl=PlatformAdaptor(**{'session':base.session})
       pl.store_platform_data(data=platform_data)
         
-   with open(seqrun_json, 'r') as json_data:
-     seqrun_data=json.load(json_data)
-     sra=SeqrunAdaptor(**{'session':base.session})
-     sra.store_seqrun_and_attribute_data(data=seqrun_data)
-    base.stop_session()
+    with open(seqrun_json, 'r') as json_data:
+      seqrun_data=json.load(json_data)
+      sra=SeqrunAdaptor(**{'session':base.session})
+      sra.store_seqrun_and_attribute_data(data=seqrun_data)
+      base.close_session()
 
   def tearDown(self):
-     Base.metadata.create_all(self.engine)
+     Base.metadata.drop_all(self.engine)
+     os.remove(self.dbname)
+     shutil.rmtree(self.md5_out_path, ignore_errors=False, onerror=None)
 
-   
+  def test_find_new_seqrun_dir(self):
+    valid_seqrun_dir=find_new_seqrun_dir(path=self.path,dbconfig=self.dbconfig) 
+    self.assertEqual(list(valid_seqrun_dir.keys())[0],'seqrun1')
+
+  def test_calculate_file_md5(self):
+    valid_seqrun_dir=find_new_seqrun_dir(path=self.path,dbconfig=self.dbconfig) 
+    new_seqrun_and_md5=calculate_file_md5(seqrun_info=valid_seqrun_dir, md5_out=self.md5_out_path, seqrun_path=self.path)
+    md5_file_name=new_seqrun_and_md5['seqrun1']
+    with open(md5_file_name, 'r') as json_data:
+      md5_data=json.load(json_data)
+    shutil.rmtree(self.md5_out_path, ignore_errors=False, onerror=None)
+    os.mkdir(self.md5_out_path)
+    self.assertEqual(md5_data["RTAComplete.txt"], "c514939fdd61df26b103925a5122b356")
+
+  def test_load_seqrun_files_to_db(self):
+    valid_seqrun_dir=find_new_seqrun_dir(path=self.path,dbconfig=self.dbconfig)
+    new_seqrun_and_md5=calculate_file_md5(seqrun_info=valid_seqrun_dir, md5_out=self.md5_out_path, seqrun_path=self.path) 
+    load_seqrun_files_to_db(seqrun_info=valid_seqrun_dir, seqrun_md5_info=new_seqrun_and_md5, dbconfig=self.dbconfig)
+
+if __name__=='__main__':
+  unittest.main()
+
