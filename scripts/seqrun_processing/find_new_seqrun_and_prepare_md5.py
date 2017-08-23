52a18e03b77e0fdd7c03b6e706f7533a137f840d
Avik Datta <avik.datta@imperial.ac.uk>
2017-08-23 16:58:21 +0100

    added pipeline seeding option for new seqrun

--- scripts/seqrun_processing/find_new_seqrun_and_prepare_md5.py
+++ scripts/seqrun_processing/find_new_seqrun_and_prepare_md5.py
@@ -1,7 +1,7 @@
 import argparse
 from igf_data.task_tracking.igf_slack import IGF_slack
 from igf_data.task_tracking.igf_asana import IGF_asana
-from igf_data.process.seqrun_processing.find_and_process_new_seqrun import find_new_seqrun_dir, calculate_file_md5, load_seqrun_files_to_db
+from igf_data.process.seqrun_processing.find_and_process_new_seqrun import find_new_seqrun_dir, calculate_file_md5, load_seqrun_files_to_db, seed_pipeline_table_for_new_seqrun
 
 parser=argparse.ArgumentParser()
 parser.add_argument('-p','--seqrun_path', required=True, help='Seqrun directory path')
@@ -10,6 +10,7 @@ parser.add_argument('-d','--dbconfig_path', required=True, help='Database config
 parser.add_argument('-s','--slack_config', required=True, help='Slack configuration json file')
 parser.add_argument('-a','--asana_config', required=True, help='Asana configuration json file')
 parser.add_argument('-i','--asana_project_id', required=True, help='Asana project id')
+parser.add_argument('-n','--pipeline_name', required=True, help='IGF pipeline name')
 args=parser.parse_args()
 
 seqrun_path=args.seqrun_path
@@ -18,6 +19,7 @@ dbconfig_path=args.dbconfig_path
 slack_config=args.slack_config
 asana_config=args.asana_config
 asana_project_id=args.asana_project_id
+pipeline_name=args.pipeline_name
 
 slack_obj=IGF_slack(slack_config=slack_config)
 asana_obj=IGF_asana(asana_config=asana_config, asana_project_id=asana_project_id)
@@ -27,6 +29,7 @@ try:
   if len(new_seqruns.keys()) > 0:
     new_seqrun_files_and_md5=calculate_file_md5(seqrun_info=new_seqruns, md5_out=md5_path,seqrun_path=seqrun_path)
     load_seqrun_files_to_db(seqrun_info=new_seqruns, seqrun_md5_info=new_seqrun_files_and_md5, dbconfig=dbconfig_path)
+    seed_pipeline_table_for_new_seqrun(seqrun_info=new_seqruns, pipeline_name=pipeline_name, dbconfig=dbconfig_path)
 
     for seqrun_name in new_seqruns.keys():
       message='found new sequencing run {0}'.format(seqrun_name)
