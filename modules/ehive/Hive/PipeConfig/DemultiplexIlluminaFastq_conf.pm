f69ce8b39a8c2dd96a1840388cb61cdce7138505
Avik Datta <reach4avik@yahoo.com>
2017-09-11 14:24:01 +0100

    new analysis for file transfer

--- modules/ehive/Hive/PipeConfig/DemultiplexIlluminaFastq_conf.pm
+++ modules/ehive/Hive/PipeConfig/DemultiplexIlluminaFastq_conf.pm
@@ -17,12 +17,29 @@ sub pipeline_analyses {
   my @pipeline;
   
   push @pipeline, {
-    -logic_name => 'find_new_sequencing_runs',
-    -module => 'ehive.runnable.jobfactory.PipeseedFactory',
+    -logic_name  => 'find_new_sequencing_runs',
+    -module      => 'ehive.runnable.jobfactory.PipeseedFactory',
+    -language    => 'python3',
+    -meadow_type => 'LOCAL',
+    -flow_into => {
+      2 => ['seqrun_transfer_factory'],    
+    },
+  };
+  push @pipeline, {
+    -logic_name  => 'seqrun_transfer_factory',
+    -module      => 'ehive.runnable.jobfactory.SeqrunFileFactory',
+    -language    => 'python3',
+    -meadow_type => 'LOCAL',
+    -flow_into => {
+      2 => ['transfer_seqrun_file'],    
+    },
+  };
+  push @pipeline, {
+    -logic_name  => 'transfer_seqrun_file',
+    -module      => 'ehive.runnable.proccess.TransferAndCheckRemoteFile',
     -language    => 'python3',
     -meadow_type => 'LOCAL',
   };
-  
   
  
   return \@pipeline
