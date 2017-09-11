109c3bea6cb8e5a3bd58d8cbdcdd4644541b9cd6
Avik Datta <reach4avik@yahoo.com>
2017-09-11 19:35:26 +0100

    added parameter parsing for config

--- modules/ehive/Hive/PipeConfig/DemultiplexIlluminaFastq_conf.pm
+++ modules/ehive/Hive/PipeConfig/DemultiplexIlluminaFastq_conf.pm
@@ -11,6 +11,20 @@ use warnings;
 use Bio::EnsEMBL::Hive::PipeConfig::HiveGeneric_conf;
 use base ('ehive::Hive::PipeConfig::IGFBasePipe_conf');
 
+sub default_options {
+  my ($self) = @_;
+  return {
+    %{ $self->SUPER::default_options() },
+    'pipeline_name' => 'DemultiplexIlluminaFastq',
+    'seed_id_label' => undef,
+    'seqrun_id_label' => undef,
+    'seqrun_source' => undef,
+    'seqrun_server' => undef,
+    'seqrun_md5_type' => undef,
+    'seqrun_local_dir' => undef,
+    'checksum_type' => undef,
+  };
+}
 
 sub pipeline_analyses {
   my ($self) = @_;
@@ -21,6 +35,11 @@ sub pipeline_analyses {
     -module      => 'ehive.runnable.jobfactory.PipeseedFactory',
     -language    => 'python3',
     -meadow_type => 'LOCAL',
+    -parameters  =>{
+      'pipeline_name' => $self->o('pipeline_name'),
+      'seed_id_label' => $self->o('seed_id_label'),
+      'seqrun_id_label'=> $self->o('seqrun_id_label'),
+    },
     -flow_into => {
       2 => ['seqrun_transfer_factory'],    
     },
@@ -30,6 +49,12 @@ sub pipeline_analyses {
     -module      => 'ehive.runnable.jobfactory.SeqrunFileFactory',
     -language    => 'python3',
     -meadow_type => 'LOCAL',
+    -parameters  => {
+      'seqrun_source' => $self->o('seqrun_source'),
+      'seqrun_server' => $self->o('seqrun_server'),
+      'seqrun_user'   => $self->o('seqrun_user'),
+      'seqrun_md5_type' => $self->o('seqrun_md5_type'),
+    },
     -flow_into => {
       2 => ['transfer_seqrun_file'],    
     },
@@ -39,6 +64,12 @@ sub pipeline_analyses {
     -module      => 'ehive.runnable.proccess.TransferAndCheckRemoteFile',
     -language    => 'python3',
     -meadow_type => 'LOCAL',
+    -parameters => {
+      'seqrun_source' => $self->o('seqrun_source'),
+      'seqrun_server' => $self->o('seqrun_server'),
+      'seqrun_local_dir' => $self->o('seqrun_local_dir'),
+      'checksum_type' => $self->o('checksum_type'),
+    },
   };
   
  
