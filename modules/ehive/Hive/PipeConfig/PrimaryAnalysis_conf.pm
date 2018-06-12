7a5a1ab2b435b6b3832146b15fd2585a24e67e8b
Avik Datta <reach4avik@yahoo.com>
2018-06-12 15:30:12 +0100

    added basic dataflow for analysis pipeline

--- modules/ehive/Hive/PipeConfig/PrimaryAnalysis_conf.pm
+++ modules/ehive/Hive/PipeConfig/PrimaryAnalysis_conf.pm
@@ -16,6 +16,9 @@ sub default_options {
   return {
     %{ $self->SUPER::default_options() },                                       # here we inherit anything from the base class
     'pipeline_name'       => 'PrimaryAnalysis',
+    'pipeseed_mode'       => 'alignment',
+    'singlecell_source'   => 'TRANSCRIPTOMIC_SINGLE_CELL',
+    'tenx_chemistry'      => 'TENX'
     'base_work_dir'       => undef,
     'base_results_dir'    => undef,
     'seqrun_user'         => undef,
@@ -33,6 +36,8 @@ sub pipeline_wide_parameters {
     my ($self) = @_;
     return {
         %{$self->SUPER::pipeline_wide_parameters},                              # here we inherit anything from the base class
+        'singlecell_source' = $self->o('singlecell_source'),
+        'tenx_chemistry' = $self->o('tenx_chemistry'),
     };
 }
 
@@ -40,6 +45,48 @@ sub pipeline_analyses {
   my ($self) = @_;
   my @pipeline;
   
+  ## collect all experiment seeds
+  push @pipeline, {
+    -logic_name  => 'find_new_sequencing_runs',
+    -module      => 'ehive.runnable.jobfactory.PipeseedFactory',
+    -language    => 'python3',
+    -meadow_type => 'LOCAL',
+    -parameters  => {
+      'pipeline_name' => $self->o('pipeline_name'),
+      'pipeseed_mode' => $self->o('pipeseed_mode'),
+    },
+    -flow_into => {
+        2 => WHEN('#singlecell_chemistry# eq #tenx_chemistry# and #library_source# eq #singlecell_source#' => ['run_cellranger_count_for_experiment'],
+                 ELSE ['mark_experiment_finished'],),
+    },
+  };
+  
+  push @pipeline, {
+    -logic_name  => 'run_cellranger_count_for_experiment',
+    -module      => 'ehive.runnable.process.alignment.RunCellrangerCount',
+    -language    => 'python3',
+    -meadow_type => 'PBSPro',
+    -rc_name     => '2Gb',
+    -analysis_capacity => 1,
+    -parameters  => {
+      'cellranger_exe'     => $self->o('cellranger_exe'),
+      'cellranger_options' => $self->o('cellranger_options'),
+      },
+    -flow_into   => {
+        1 => ['mark_experiment_finished'],  
+      },
+  };
+  
+  push @pipeline, {
+      -logic_name   => 'mark_experiment_finished',
+      -module       => 'ehive.runnable.process.ChangePipelineSeedStatus',
+      -language     => 'python3',
+      -meadow_type  => 'LOCAL',
+      -parameters   => {
+        'new_status'    => 'FINISHED',
+        'pipeline_name' => $self->o('pipeline_name'),
+        },
+  };
   return \@pipeline;
 }
 
