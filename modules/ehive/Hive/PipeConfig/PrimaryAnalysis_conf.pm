22f291fa932ceab2a8feab6df2095f7151eec550
Avik Datta <reach4avik@yahoo.com>
2018-06-20 12:54:00 +0100

    adding analysis for cram conversion

--- modules/ehive/Hive/PipeConfig/PrimaryAnalysis_conf.pm
+++ modules/ehive/Hive/PipeConfig/PrimaryAnalysis_conf.pm
@@ -29,6 +29,10 @@ sub default_options {
     'cellranger_param'    => undef,
     'multiqc_options'     => '{"--zip-data-dir" : ""}',
     'cleanup_bam_dir'     => 0,
+    'cram_type'           => 'ANALYSIS_CRAM',
+    'reference_fasta_type'        => 'GENOME_FASTA',
+    'cellranger_collection_table' => 'experiment',
+    'cellranger_analysis_name'    => 'cellranger_count',
   };
 }
 
@@ -74,6 +78,28 @@ sub pipeline_analyses {
       'base_work_dir'      => $self->o('base_work_dir'),
       'base_results_dir'   => $self->o('base_results_dir'),
       },
+    -flow_into   => {
+        1 => ['convert_bam_to_cram'],  
+      },
+  };
+  
+  push @pipeline, {
+    -logic_name  => 'convert_bam_to_cram',
+    -module      => 'ehive.runnable.process.alignment.ConvertBamToCram',
+    -language    => 'python3',
+    -meadow_type => 'PBSPro',
+    -rc_name     => '2Gb',
+    -analysis_capacity => 1,
+    -parameters  => {
+        'bam_file'        => '#bam_file#',
+        'base_result_dir' => $self->o('base_results_dir'),
+        'collection_name' => '#experment_igf_id#',
+        'collection_type' => $self->o('cram_type'),
+        'collection_table'=> $self->o('cellranger_collection_table'),
+        'analysis_name'   => $self->o('cellranger_analysis_name'),
+        'tag_name'        => '#species_name#',
+        'reference_type'  => $self->o('reference_fasta_type'),
+     },
     -flow_into   => {
         1 => ['mark_experiment_finished'],  
       },
