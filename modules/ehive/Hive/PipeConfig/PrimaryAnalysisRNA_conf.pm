97a8f4511fe479503792d3875276e39cf4a705d0
Avik Datta <reach4avik@yahoo.com>
2018-10-24 22:37:46 +0100

    fixed passing bam file to dataflow

--- modules/ehive/Hive/PipeConfig/PrimaryAnalysisRNA_conf.pm
+++ modules/ehive/Hive/PipeConfig/PrimaryAnalysisRNA_conf.pm
@@ -461,7 +461,7 @@ sub pipeline_analyses {
     -rc_name     => '4Gb',
     -analysis_capacity => 2,
     -parameters  => {
-      'input_files'    => ['#bam_file#'],
+      'input_files'    => '#bam_files#',
       'java_exe'       => $self->o('java_exe'),
       'java_param'     => $self->o('java_param'),
       'picard_jar'     => $self->o('picard_jar'),
@@ -471,7 +471,7 @@ sub pipeline_analyses {
       'reference_type' => $self->o('reference_fasta_type'),
      },
     -flow_into   => {
-        1 => ['picard_base_dist_summary_for_star'],
+        1 => {'picard_base_dist_summary_for_star => { 'input_files' => '#input_files#'}},
       },
   };
   
@@ -485,7 +485,7 @@ sub pipeline_analyses {
     -rc_name     => '4Gb',
     -analysis_capacity => 2,
     -parameters  => {
-      'input_files'    => ['#bam_file#'],
+      'input_files'    => '#input_files#',
       'java_exe'       => $self->o('java_exe'),
       'java_param'     => $self->o('java_param'),
       'picard_jar'     => $self->o('picard_jar'),
@@ -508,7 +508,7 @@ sub pipeline_analyses {
     -rc_name     => '4Gb',
     -analysis_capacity => 2,
     -parameters  => {
-      'input_files'    => ['#bam_file#'],
+      'input_files'    => '#input_files#',
       'java_exe'       => $self->o('java_exe'),
       'java_param'     => $self->o('java_param'),
       'picard_jar'     => $self->o('picard_jar'),
@@ -532,7 +532,7 @@ sub pipeline_analyses {
     -rc_name     => '4Gb',
     -analysis_capacity => 2,
     -parameters  => {
-      'input_files'    => ['#bam_file#'],
+      'input_files'    => '#input_files#',
       'java_exe'       => $self->o('java_exe'),
       'java_param'     => $self->o('java_param'),
       'picard_jar'     => $self->o('picard_jar'),
@@ -556,7 +556,7 @@ sub pipeline_analyses {
     -rc_name     => '4Gb',
     -analysis_capacity => 2,
     -parameters  => {
-      'input_files'    => ['#bam_file#'],
+      'input_files'    => '#input_files#',
       'java_exe'       => $self->o('java_exe'),
       'java_param'     => $self->o('java_param'),
       'picard_jar'     => $self->o('picard_jar'),
@@ -581,7 +581,7 @@ sub pipeline_analyses {
     -rc_name     => '2Gb4t',
     -analysis_capacity => 2,
     -parameters  => {
-      'bam_file'         => '#bam_file#',
+      'bam_file'         => '#expr( ${#input_files#}[0] )expr#',
       'samtools_command' => 'flagstat',
       'base_work_dir'    => $self->o('base_work_dir'),
       'reference_type'   => $self->o('reference_fasta_type'),
@@ -603,7 +603,7 @@ sub pipeline_analyses {
     -rc_name     => '2Gb',
     -analysis_capacity => 2,
     -parameters  => {
-      'bam_file'         => '#bam_file#',
+      'bam_file'         => '#expr( ${#input_files#}[0] )expr#',
       'samtools_command' => 'idxstats',
       'base_work_dir'    => $self->o('base_work_dir'),
       'reference_type'   => $self->o('reference_fasta_type'),
