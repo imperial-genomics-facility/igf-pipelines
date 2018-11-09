6d0ef537deba6cb4258cda6b5025ab2ad64382e0
Avik Datta <reach4avik@yahoo.com>
2018-11-09 11:43:08 +0000

    adding output prefix for picard tools

--- modules/ehive/Hive/PipeConfig/PrimaryAnalysisRNA_conf.pm
+++ modules/ehive/Hive/PipeConfig/PrimaryAnalysisRNA_conf.pm
@@ -264,6 +264,7 @@ sub pipeline_analyses {
          'RGCN'       => 'Imperial Genomics Facility',
          'SORT_ORDER' => 'coordinate',
          },
+      'output_prefix'  => '#run_igf_id#'.'_'.'#RG#'.'_'.'genomic',
      },
     -flow_into => {
           1 => [ '?accu_name=star_aligned_genomic_bam&accu_address={experiment_igf_id}{seed_date_stamp}[]&accu_input_variable=analysis_files' ],
@@ -295,6 +296,7 @@ sub pipeline_analyses {
          'RGCN'       => 'Imperial Genomics Facility',
          'SORT_ORDER' => 'unsorted',
          },
+      'output_prefix'  => '#run_igf_id#'.'_'.'#RG#'.'_'.'transcriptomic',
      },
     -flow_into => {
           1 => [ '?accu_name=star_aligned_trans_bam&accu_address={experiment_igf_id}{seed_date_stamp}[]&accu_input_variable=analysis_files' ],
@@ -531,6 +533,7 @@ sub pipeline_analyses {
       'picard_command' => 'CollectAlignmentSummaryMetrics',
       'base_work_dir'  => $self->o('base_work_dir'),
       'reference_type' => $self->o('reference_fasta_type'),
+      'output_prefix'  => '#experiment_igf_id#'.'_'.'#ALN#',
      },
     -flow_into   => {
         1 => ['picard_base_dist_summary_for_star'],
@@ -554,6 +557,7 @@ sub pipeline_analyses {
       'picard_command' => 'CollectBaseDistributionByCycle',
       'base_work_dir'  => $self->o('base_work_dir'),
       'reference_type' => $self->o('reference_fasta_type'),
+      'output_prefix'  => '#experiment_igf_id#'.'_'.'#BS#',
      },
     -flow_into   => {
         1 => ['picard_gc_bias_summary_for_star'],
@@ -576,6 +580,7 @@ sub pipeline_analyses {
       'picard_command' => 'CollectGcBiasMetrics',
       'base_work_dir'  => $self->o('base_work_dir'),
       'reference_type' => $self->o('reference_fasta_type'),
+      'output_prefix'  => '#experiment_igf_id#'.'_'.'#GC#',
      },
     -flow_into   => {
         1 => ['picard_qual_dist_summary_for_star'],
@@ -599,6 +604,7 @@ sub pipeline_analyses {
       'picard_command' => 'QualityScoreDistribution',
       'base_work_dir'  => $self->o('base_work_dir'),
       'reference_type' => $self->o('reference_fasta_type'),
+      'output_prefix'  => '#experiment_igf_id#'.'_'.'#QS#',
      },
     -flow_into   => {
         1 => ['picard_rna_metrics_summary_for_star'],
@@ -623,6 +629,7 @@ sub pipeline_analyses {
       'base_work_dir'  => $self->o('base_work_dir'),
       'reference_type' => $self->o('reference_fasta_type'),
       'reference_refFlat' => $self->o('reference_refFlat'),
+      'output_prefix'  => '#experiment_igf_id#'.'_'.'#RNA#',
      },
     -flow_into   => {
         1 => ['samtools_flagstat_summary_for_star'],
